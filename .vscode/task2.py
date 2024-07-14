343749658cc609d531694f4930a2d64c1abef371
Oleksandr Tesler <tesler_2000@yahoo.com>
2024-07-14 12:20:18 +0300

    1

---
+++ .vscode/task2.py
@@ -0,0 +1,15 @@
+from datetime import datetime, date, timedelta
+today = date.today()
+users = [
+    {"name": "Bill Gates", "birthday": "1955.3.25"},
+    {"name": "Steve Jobs", "birthday": "1955.3.21"},
+    {"name": "Jinny Lee", "birthday": "1956.3.22"},
+    {"name": "Sarah Lee", "birthday": "1957.3.23"},
+    {"name": "Jonny Lee", "birthday": "1958.3.22"},
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+
+for user in users:
+        birthday_this_year = user["birthday"].replace(year=today.year)
+        print(birthday_this_year)
