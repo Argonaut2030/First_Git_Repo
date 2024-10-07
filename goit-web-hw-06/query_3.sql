4ff43aee0aa5c3220d875be6c2dcfacf6782a253
Oleksandr Tesler <tesler_2000@yahoo.com>
2024-10-07 23:01:53 +0300

    hw 07

--- goit-web-hw-06/query_3.sql
+++ goit-web-hw-06/query_3.sql
@@ -1,11 +1,10 @@
 -- Знайти середній бал у групах з певного предмета.
 SELECT 
-    s.id, 
-    s.fullname, 
+    s.group_id, 
     ROUND(AVG(g.grade), 2) AS average_grade
 FROM grades g
 JOIN students s ON s.id = g.student_id
-where g.subject_id = 1
-GROUP BY s.id
+where g.subject_id = 2
+GROUP BY s.group_id
 ORDER BY average_grade DESC
-LIMIT 1;
+;
